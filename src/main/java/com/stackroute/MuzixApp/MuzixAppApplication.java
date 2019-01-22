2cd15dde02674e3362625a4a2e7e41ffc8844551
Komal_R <komalr984@gmail.com>
2019-01-22 17:53:39 +0530

    ListenerAndRunnerCommit

--- src/main/java/com/stackroute/MuzixApp/MuzixAppApplication.java
+++ src/main/java/com/stackroute/MuzixApp/MuzixAppApplication.java
@@ -1,14 +1,35 @@
 package com.stackroute.MuzixApp;
 
+import com.stackroute.MuzixApp.domain.Track;
+import com.stackroute.MuzixApp.repository.TrackRepository;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class MuzixAppApplication {
+public class MuzixAppApplication implements ApplicationListener<ContextRefreshedEvent> , CommandLineRunner {
 
+	@Autowired
+	TrackRepository trackRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MuzixAppApplication.class, args);
 	}
 
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		trackRepository.save(new Track(1,"abcde","Nosong"));
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		trackRepository.save(new Track(2,"abcdef","Nosong"));
+	}
 }
 
